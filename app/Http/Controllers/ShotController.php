cafa7261cb6c125595ee9a187b88871d36d42771
satpro-hub <satpro@yandex.ru>
2023-04-25 23:06:38 +0300

    сделал работающий драфт

--- app/Http/Controllers/ShotController.php
+++ app/Http/Controllers/ShotController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Browsershot\Browsershot;
 
 class ShotController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return response(["result" => "ok"]);
+        $url = $request->input('url');
+        $image_name = $request->input('image_name', 'screenshot.jpg');
+        $width = $request->input('width', 1920);
+        $height = $request->input('height', 1080);
+
+        $obj = Browsershot::url($url)
+            ->noSandbox()
+            ->windowSize($width, $height)
+            ->setChromePath("/usr/bin/chromium-browser");
+        $messages = $obj->consoleMessages();
+        $obj->save($image_name);
+        return response(["messages" => $messages]);
     }
 }
